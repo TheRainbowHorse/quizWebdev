48295da3811a3295ef9e2522554ea6fba158864e
TheRainbowHorse <therainbowhorse3@gmail.com>
2023-12-22 19:24:29 +0200

    pr 5-3 saving answers and thanks added

--- script/script.js
+++ script/script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const formAnswers = document.querySelector('#formAnswers');
     const btnPrev = document.querySelector('#prev');
     const btnNext = document.querySelector('#next');
+    const btnSend = document.querySelector('#send');
 
     btnOpenModal.addEventListener('click', () => {
         modalBlock.classList.add('d-block');
@@ -30,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const playTest = (questions) => {
+        const userAnswers = [];
         let questionNumber = 0;
 
         const renderQuestion = ({ question, answers, type }) => {
@@ -38,8 +40,8 @@ document.addEventListener('DOMContentLoaded', function() {
             formAnswers.innerHTML = '';
             answers.forEach((answer, index) => {
                 formAnswers.innerHTML += `
-                    <div class="answers-item d-flex flex-column">
-                        <input type="${type}" id="answerItem${index + 1}" name="answer" class="d-none">
+                    <div class="answers-item d-flex justify-content-center">
+                        <input type="${type}" id="answerItem${index + 1}" name="answer" class="d-none" value="${answer.title}">
                         <label for="answerItem${index + 1}" class="d-flex flex-column justify-content-between">
                             <img class="answerImg" src="${answer.url}" alt="burger">
                             <span>${answer.title}</span>
@@ -50,32 +52,74 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         renderQuestion(questions[questionNumber]);
 
-        btnNext.addEventListener('click', () => {
+        const checkAnswer = () => {
+            const obj = {};
+            const inputs = [...formAnswers.elements].filter((input) => input.checked || input.id == 'phoneNumber');
+            inputs.forEach((input, index) => {
+                if (questionNumber >= 0 && questionNumber <= questions.length - 1) {
+                    obj[`${index}_${questions[questionNumber].question}`] = input.value;
+                }
+                if (questionNumber == questions.length) {
+                    obj[`Номер телефона`] = input.value;
+                }
+            });
+            userAnswers.push(obj);
+            console.log("userAnswers:", userAnswers)
+        }
+        
+        btnNext.onclick = () => {
+            checkAnswer();
             questionNumber++;
-            renderQuestion(questions[questionNumber]);
+            if (questionNumber >= 0 && questionNumber <= questions.length - 1) {
+                renderQuestion(questions[questionNumber]);
+            }
             renderControlBtns();
-        });
+        };
 
-        btnPrev.addEventListener('click', () => {
+        btnPrev.onclick = () => {
             questionNumber--;
-            renderQuestion(questions[questionNumber]);
+            if (questionNumber >= 0 && questionNumber <= questions.length - 1) {
+                renderQuestion(questions[questionNumber]);
+            }
             renderControlBtns();
-        });
+        };
+
+        btnSend.onclick = () => {
+            checkAnswer();
+            questionNumber++;
+            renderControlBtns();
+        };
 
         const renderControlBtns = () => {
-            if (questionNumber == 0) {
-                btnPrev.classList.add('hide');
-                btnNext.classList.remove('hide');
+            if (questionNumber >= 0 && questionNumber <= questions.length - 1) {
+                btnSend.classList.add('d-none');
+                btnNext.classList.remove('d-none');
+                btnPrev.classList.remove('d-none');
             }
-            else if (questionNumber == questions.length - 1) {
-                btnNext.classList.add('hide');
-                btnPrev.classList.remove('hide');
-            }
-            else {
-                btnPrev.classList.remove('hide');
-                btnNext.classList.remove('hide');
-            }
+            switch (questionNumber) {
+                case 0:
+                    btnPrev.classList.add('d-none');
+                    break;
+                case questions.length:
+                    btnPrev.classList.add('d-none');
+                    btnNext.classList.add('d-none');
+                    btnSend.classList.remove('d-none');
 
+                    formAnswers.innerHTML = `
+                        <div class="form-group">
+                            <label for="phoneNumber">Enter your number</label>
+                            <input type="phone" class="form-control" id="phoneNumber">
+                        </div>
+                    `;
+                    break;
+                case questions.length + 1:
+                    questionTitle.textContent = 'Спасибо за пройденный тест!';
+                    formAnswers.textContent = '';
+                    setTimeout(() => {
+                        modalBlock.classList.remove('d-block');
+                    }, 2000);
+                    break;
+            }
         }
         renderControlBtns();
     }
